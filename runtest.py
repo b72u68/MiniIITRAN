6988ffb3e0019276dfabe5544968f1ac608f53bb
b72u68 <mdinh@hawk.iit.edu>
2022-09-01 09:20:09 -0500

    Added test cases + updated test program

--- runtest.py
+++ runtest.py
@@ -1,24 +1,26 @@
 from os import listdir
 from os.path import isfile, join
-from subprocess import PIPE, Popen
+import subprocess
 
-
-testdir = "./tests"
+testdir = "tests"
 testfiles = [f for f in listdir(testdir) if isfile(join(testdir, f)) and f.endswith(".iit")]
 
 for testfile in testfiles:
     with open(join(testdir, testfile)) as f:
-        if "synerr" not in testfile:
+        if testfile.startswith("synerr"):
+            expected_result = ""
+            expected_exit_code = 1
+        elif testfile.startswith("semerr"):
+            expected_result = ""
+            expected_exit_code = 2
+        else:
             lines = f.readlines()
             expected_result = lines[0].split(":")[1].strip("\n").strip(" ")
-        else:
-            expected_result = ""
-        process = Popen(["./iitran", join(testdir, testfile)], stdout=PIPE, stderr=None)
-        result, error = process.communicate()
-        if error:
-            raise Exception(f"{testfile}: encountered error {error}")
-        result = result.decode("utf-8").strip("\n").strip(" ")
-        if result != expected_result:
-            raise Exception(f"{testfile}: expected result to be '{expected_result}', got '{result}'")
+            expected_exit_code = 0
+        process = subprocess.run(["./iitran", join(testdir, testfile)], capture_output=True, text=True)
+        exit_code = process.returncode
+        result = process.stdout.strip("\n").strip(" ")
+        if result != expected_result or expected_exit_code != exit_code:
+            print(f"{testfile}: expected result to be '{expected_result}' with exit code {expected_exit_code}, got '{result}' with exit code {exit_code}")
         else:
             print(f"{testfile}: passed")
